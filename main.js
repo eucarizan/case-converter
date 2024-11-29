2bd8f630ee49a3977c1a0b231a06a0903ca1b4b2
eucarizan <eucarizan@gmail.com>
2024-11-29 18:45:51 +0800

    1: case converter - feature, lorem ipsum

--- main.js
+++ main.js
@@ -29,12 +29,22 @@ function toSentenceCase() {
 
 function download() {
     let element = document.createElement('a');
-    element.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(textArea.value);
+    element.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(textarea.value);
     element.download = 'text.txt';
     element.style.display = 'none';
     document.body.appendChild(element);
     element.click();
     document.body.removeChild(element);
+}
+
+function addText() {
+    textarea.value = "lorem ipsum is simply dummy text of the printing and typesetting industry. " +
+      "lorem ipsum has been the industry's standard dummy text ever since the 1500s, " +
+      "when an unknown printer took a gallery of type and scrambled it to make a type specimen book. " +
+      "it has survived not only five centuries, but also the leap into electronic typesetting, " + 
+      "remaining essentially unchanged. it was popularised in the 1960s with the release of letraset " +
+      "sheets containing lorem ipsum passages, and more recently with desktop publishing software " +
+      "like aldus pagemaker including versions of lorem ipsum."
 }
 
 // Add event listeners to the buttons
@@ -43,3 +53,4 @@ document.getElementById('lower-case').addEventListener('click', toLowerCase);
 document.getElementById('proper-case').addEventListener('click', toProperCase);
 document.getElementById('sentence-case').addEventListener('click', toSentenceCase);
 document.getElementById('save-text-file').addEventListener('click', download);
+document.getElementById('lorem-ipsum').addEventListener('click', addText);
